a586a3670b471201d0548841155c494d69c6fa4c
jerrytwmseo <jerry.twm.seo@gmail.com>
2020-05-08 17:54:01 -0400

    add visible components

--- empty/app/src/main/java/com/jerry/empty/OnDemandActivity.java
+++ empty/app/src/main/java/com/jerry/empty/OnDemandActivity.java
@@ -1,6 +1,12 @@
 package com.jerry.empty;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +16,8 @@ public class OnDemandActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.on_demand);
+        Spinner spinner = findViewById(R.id.httpSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.http_protocol,android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
